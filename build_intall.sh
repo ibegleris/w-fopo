e4336e8907c4558b59a1f8cf82db2181edc20ae4
Ioannis Begleris <i.begleris@soton.ac.uk>
2017-08-02 12:19:22 +0100

    made a build intall bash file

--- build_intall.sh
+++ build_intall.sh
@@ -1,6 +1,7 @@
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 rm Miniconda3-latest-Linux-x86_64.sh
+sleep 5
 source ~/.bashrc
 conda update conda -y
 conda config --add channels intel
