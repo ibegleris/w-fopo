ad948b4349b53d896a52ce70a5cafd2e68145a0c
Ioannis Begleris <i.begleris@soton.ac.uk>
2017-08-02 12:04:55 +0100

    made a build intall bash file

---
+++ build_intall.sh
@@ -0,0 +1,10 @@
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
+source ~/.bashrc
+conda update conda -y
+conda config --add channels intel
+conda create -n intel intelpython3_core python=3
+source activate intel
+conda install numpy scipy matplotlib pandas h5py pytables jupyter joblib numba pytest -y
+pytest unittesting_scripts.py
