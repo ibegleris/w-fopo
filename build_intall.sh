e6312327e0b81755cb9caa17d06e8695ee851631
Ioannis Begleris <i.begleris@soton.ac.uk>
2017-08-08 12:34:23 +0100

    made a better build sh file that makes sure that the conda enviroments are up to standard

--- build_intall.sh
+++ build_intall.sh
@@ -8,4 +8,13 @@ conda config --add channels intel
 conda create -n intel intelpython3_core python=3
 source activate intel
 conda install numpy scipy matplotlib pandas h5py pytables jupyter joblib numba pytest nose -y
+source deactivate
+rm -rf ../.condarc
+mv build_data/.condarc_default ../.condarc
+mv build_data/.condarc_default ../miniconda3/envs/intel/.condarc
+rmdir build_data
+conda update conda -y
+conda install numpy scipy matplotlib jupyter pandas h5py pytables jupyter numba pytest nose -y
+conda install python=3.6 -y
+source activate intel
 pytest unittesting_scripts.py
